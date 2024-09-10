73b8350008061f5e6bb93ea316aebf4295716a76
J <J.@qq.com>
2024-09-10 18:39:31 +0800

    feat(文本预处理功能开发)

--- 3122004739/src/main/java/TextParser.java
+++
@@ -1,7 +0,0 @@
-/**
- * 文本预处理类
- */
-public class TextParser {
-
-
-}
---
+++ 3122004739/src/main/java/TextParserUtil.java
@@ -0,0 +1,17 @@
+/**
+ * 文本预处理类
+ */
+public class TextParserUtil {
+
+    /**
+     * 文本预处理
+     * @param text 文本
+     * @return 处理后的文本
+     */
+    public static String clean(String text) {
+        String punctuationRegex = "[，。/；’【】、·！《》？：“{}|@#￥%…^&*+—.?!~`\"';:\n\r]+";
+        text = text.replaceAll(punctuationRegex, "");
+        return text;
+    }
+
+}
